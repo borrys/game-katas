32ebf135de963a7e6620d84db8f5a3d6eb9015de
BorryS <andrzej.kimel@gmail.com>
2016-05-23 21:27:40 +0200

    simple animation with quil

--- src/game_katas/core.clj
+++ src/game_katas/core.clj
@@ -1,7 +1,26 @@
 (ns game-katas.core
-  (:gen-class))
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn setup []
+  {:pos {:x 100 :y 100}
+   :dir 1})
+
+(defn move [pos]
+  (update-in pos [:x] #(+ 5 %)))
+
+
+(defn update-state [state]
+  (update-in state [:pos] move))
+
+(defn draw [{{x :x y :y} :pos}]
+  (q/background 200 200 200)
+  (q/rect x y 50 50 10))
+
+(q/defsketch my-sketch
+  :title "Hello, quil!"
+  :size [800, 600]
+  :setup setup
+  :update update-state
+  :draw draw
+  :middleware [m/fun-mode])
