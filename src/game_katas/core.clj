25838faa97357d406119d1cf128d5e0d06580a0e
BorryS <andrzej.kimel@gmail.com>
2016-05-28 19:18:30 +0200

    dummy progress-bar; moveable 'player' object

--- src/game_katas/core.clj
+++ src/game_katas/core.clj
@@ -9,18 +9,40 @@
   ["S........##"
    "...##.#...#"
    ".#.#..#..#E"
-   "##.####...."])
+   "##.####..#."
+   "...#......."])
 
+(def version (atom 1))
 
 (defn setup []
-  (parse level))
+  (-> (parse level)
+      (assoc :version @version)
+      (assoc :value 80)
+      (assoc :dir :up)
+      (assoc :pos [5 5])))
 
 (defn move [pos]
   (update-in pos [:x] #(+ 5 %)))
 
-
 (defn update-state [state]
-  (update-in state [:pos] move))
+  (if (= @version (:version state))
+    state
+    (setup)))
+
+(defn handler [key]
+  (condp = key
+    (keyword " ") [[:value #(rem (inc %) 100)]]
+    :up {:pos #(map + % [0 -1]) :dir (fn [_] :up)}
+    :down {:pos #(map + % [0 1]) :dir (fn [_] :down)}
+    :left {:pos #(map + % [-1 0]) :dir (fn [_] :left)}
+    :right {:pos #(map + % [1 0]) :dir (fn [_] :right)}
+    []))
+
+(defn key-pressed [state {key :key}]
+  (let [ops (handler key)]
+    (reduce (fn [s [prop f]] (update-in s [prop] f))
+            state
+            ops)))
 
 (defn draw [state]
   (q/background 200 200 200)
@@ -28,6 +50,8 @@
   (render/world (:world state))
   (render/start (:start state))
   (render/end (:end state))
+  (render/progress (:value state) [1 9])
+  (render/arrow (:pos state) (:dir state))
 )
 
 (defn start []
@@ -35,6 +59,9 @@
     :title "Hello, quil!"
     :size [800, 600]
     :setup setup
- ;  :update update-state
+    :update update-state
     :draw draw
+    :key-pressed key-pressed
     :middleware [m/fun-mode]))
+
+(start)
--- src/game_katas/render.clj
+++ src/game_katas/render.clj
@@ -18,4 +18,62 @@
   (q/ellipse (+ half-tile (* tile-size x)) (+ half-tile (* tile-size y)) point-size point-size))
 
 (defn start [p] (point p [100 200 100]))
+
 (defn end [p] (point p [100 100 200]))
+
+(defn multiply [[x y] [n01 n02 n11 n12]]
+  [(+ (* x n01) (* y n11))
+   (+ (* x n02) (* y n12))])
+
+(defn triangle [size angle]
+  (let [hf (- (/ size 2))
+        nhf (- hf)
+        base-points [[0 hf]
+                     [nhf nhf]
+                     [hf nhf]]
+        cos (Math/cos angle)
+        sin (Math/sin angle)
+        rot-matrix [cos (- sin)
+                    sin cos]]
+    (map #(multiply % rot-matrix) base-points)
+    )
+  )
+
+(defn dir-to-angle [dir]
+  (let [right-angle (/ Math/PI 2)]
+    (* right-angle 
+       (case dir
+         :up 0
+         :down 2
+         :left 1
+         :right 3))))
+
+(defn arrow [[x y] direction]
+  (q/fill 250 100 100)
+  (let [angle (dir-to-angle direction)
+        size tile-size
+        sx (+ half-tile (* tile-size x))
+        sy (+ half-tile  (* tile-size y))
+        ]
+    (->> (triangle size angle)
+         (map #(vector (+ sx (first %)) (+ sy (second %))))
+         (flatten)
+         (apply q/triangle))))
+
+(defn progress [val [x y]]
+  (let [width (/ val 10)]
+    (q/fill 200 200 200)
+    (q/rect (* tile-size x) (* tile-size y) (* tile-size 10) half-tile )
+    (q/fill 100 100 250)
+    (q/rect (* tile-size x) (* tile-size y) (* tile-size width) half-tile )
+
+    ))
+
+(let [angle (dir-to-angle :up)
+      size tile-size
+      sx (+ half-tile (* tile-size 1))
+      sy (+ half-tile  (* tile-size 2))]
+    (->> (triangle size angle)
+         (map #(vector (+ sx (first %)) (+ sy (second %))))
+         (flatten)
+         (map #(/ tile-size %))))
